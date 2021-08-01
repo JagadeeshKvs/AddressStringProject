666972585669166770290313b8a0b640b39469d9
KaswarJ <Jagadeeshwar.K@gds.ey.com>
2021-08-01 15:20:45 +0530

    4

--- src/main/java/com/friday/project/addressjson/controller/AddressController.java
+++ src/main/java/com/friday/project/addressjson/controller/AddressController.java
@@ -4,14 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.friday.project.addressjson.dto.AddressDTO;
 import com.friday.project.addressjson.service.AddressResponseService;
 
-@Controller
+@RestController
 public class AddressController {
 
 	Logger logger = LoggerFactory.getLogger(AddressController.class);
@@ -23,7 +23,7 @@ public class AddressController {
 	 * Returns final address response
 	 */
 	@PostMapping("/address")
-	public ResponseEntity<Object> getAddressDetails(@RequestBody AddressDTO address) throws Exception{
+	public ResponseEntity<Object> getAddressDetails(@RequestBody AddressDTO address) throws Exception {
 		logger.info("Call to get House Number and Street for address: {}", address.getAddress());
 		return addressService.getStreetAndHouseNumber(address.getAddress());
 	}
