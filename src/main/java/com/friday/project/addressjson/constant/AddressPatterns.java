4415437aae4159c99d808df601998d0e3d3773cd
KaswarJ <Jagadeeshwar.K@gds.ey.com>
2021-08-02 13:58:02 +0530

    pattern

--- src/main/java/com/friday/project/addressjson/constant/AddressPatterns.java
+++ src/main/java/com/friday/project/addressjson/constant/AddressPatterns.java
@@ -11,7 +11,7 @@ public final class AddressPatterns {
 		PATTERN_ONE("([0-9].*) ([a-zA-Z]+) ([0-9].*)"),
 		PATTERN_TWO("(^[A-Za-z]+.*[\\s]?[0-9.]+.*?)[,]?[\\s]?([\bNo\b|\bno\b][A-Za-z].*?[0-9].*)"),
 		PATTERN_THREE("(^[A-Za-z].*[\\s]?[0-9]*?[\\d+$]) ([#]?\\d+$|\\d+[A-Za-z]+$)"),
-		PATTERN_FOUR("([a-zA-Z].* [^0-9]?)[,]?[\\s]?(\\d+$|[0-9].*$)"),
+		PATTERN_FOUR("(^[\u00C0-\u017F]?[\\s]?[a-zA-Z].* [^0-9]?)[,]?[\\s]?(\\d+$|[0-9].*$)"),
 		PATTERN_FIVE("(\\d+[a-zA-Z]?)[,]?[\\s]?([^0-9][a-zA-Z].*[\\s]?)");
 
 		private final String value;
